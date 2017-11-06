2fdf4c90d3bb7cdb7571eb8bd5d711131f104aeb
LongTa <longtv@runsystem.net>
2017-11-06 14:47:54 +0700

    add tásk

---
+++ src/main/java/vn/kyo/Tools.java
@@ -0,0 +1,14 @@
+package vn.kyo;
+
+public class Tools {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public boolean isEmail (String email) {
+		return false;
+	}
+
+}
---
+++ src/test/java/vn/kyo/ToolsTest.java
@@ -0,0 +1,14 @@
+package vn.kyo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ToolsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
